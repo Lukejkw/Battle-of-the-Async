57f595ae7d1eda6f1d57f0740741228197415031
Luke Warren <lukejkw@gmail.com>
2018-07-17 14:02:09 +0200

    Add error first promise example

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const async = require('async');
-const util = require('util');
+const async = require("async");
+const util = require("util");
+
+const to = promise =>
+  promise.then(data => [null, data]).catch(err => [err, undefined]);
 
 const step1Async = util.promisify(step1);
 const step2Async = util.promisify(step2);
 const step3Async = util.promisify(step3);
 
-doTheThingAsync()
+doTheThingsGoStyle();
+
+async function doTheThingsGoStyle() {
+  const [step1Err, result] = await to(step1Async());
+
+  if (step1Err) {
+    // Look ma! I can handle errors like the good ol' days
+    console.error('There was an error homie: ', step1Err);
+  }
+
+  // Maybe we dont care about this error, let it bubble up
+  const result2 = await step2Async(result);
+
+  // Only care for errors? 
+  const [error] = await to(step3Async(result2));
+  if(!error) {
+    console.log('We did it!')
+  } else {
+    console.error('We failed.')
+  }
+}
 
 async function doTheThingAsync() {
   try {
@@ -13,39 +36,35 @@ async function doTheThingAsync() {
     const result2 = await step2Async(result);
     const result3 = await step3Async(result2);
     console.log(result3);
-  } catch(err) {
-    console.log('err', err)
+  } catch (err) {
+    console.log("err", err);
   }
 }
 
 function doTheThingWaterfall() {
-  async.waterfall([
-    step1,
-    step2,
-    step3
-  ], (err, param) => {
+  async.waterfall([step1, step2, step3], (err, param) => {
     if (err) {
-      console.log('error', err)
+      console.log("error", err);
     }
 
-    console.log('param', param)
-  })
+    console.log("param", param);
+  });
 }
 
 function doTheThing() {
-  step1('...', function (err, value) {
+  step1("...", function(err, value) {
     if (err) {
       // Ah!!!!
     }
 
-    console.log('step1:', value)
+    console.log("step1:", value);
 
     step2(value, (err, value) => {
       if (err) {
         // Ah!!!!
       }
 
-      console.log('step2:', value)
+      console.log("step2:", value);
 
       step3(value, (err, value) => {
         if (err) {
@@ -53,23 +72,23 @@ function doTheThing() {
         }
 
         // We are done!
-        console.log('step3:', value)
-      })
-    })
-  })
+        console.log("step3:", value);
+      });
+    });
+  });
 }
 
 function step1(callback) {
-  console.log('step 1')
-  callback(null, 'goats')
+  console.log("step 1");
+  callback(null, "goats");
 }
 
 function step2(param, callback) {
   console.log(param);
-  callback(null, 'are')
+  callback(null, "are");
 }
 
 function step3(param, callback) {
   console.log(param);
-  callback(null, 'awesome')
+  callback(null, "awesome");
 }
